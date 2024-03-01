50bd58717d6e10b556f9058648bf470721f37e06
DonielM <151669456+DonielM@users.noreply.github.com>
2024-03-01 17:42:54 +0000

    Created a js file with a basic plan/ideas for what need to get the quiz running

---
+++ starter/assets/js/script.js
@@ -0,0 +1,9 @@
+//Need to make one big array opr mabye a collection of arrays for each choice for each questions.
+//Then ill need to randomize the questions  with a for loop.
+//Since the number of question is undeclared ill make 8 questions.
+
+
+// Also need to create the highscores from the the score of the past 8 questions and have a ranking system
+
+// need to use get element to use the html elemnts in js
+// Then ill need functions for the questions with iff state ments to check if the answer is right 
