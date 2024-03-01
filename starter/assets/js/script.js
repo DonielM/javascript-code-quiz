f473204be2c120b35defb87f78a4eece983cafd2
DonielM <151669456+DonielM@users.noreply.github.com>
2024-03-01 21:49:40 +0000

    Added element Ids with get Element created the functions and if statements to get the quiz to show questions and choices. Checks where answer is correct and ad to score or decreases timer accordingly.

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -50,3 +50,89 @@ const questions = [
       answer: 2,
   },
 ];
+
+let startBttn = document.getElementById("start");
+let timerDisplay = document.getElementById("time");
+let questionTitle = document.getElementById("question-title");
+let answerChoices = document.getElementById("choices");
+let initialsInput = document.getElementById("initials");
+let submitBttn = document.getElementById("submit");
+let finalScore = document.getElementById("final-score");
+
+let feedbackMessage = document.getElementById("feedback");
+let startScreen = document.getElementById("start-screen");
+let endScreen = document.getElementById("end-screen");
+let quizQuestions = document.getElementById("questions");
+
+let currentQuizQuestion = 0;
+let timeLeft = 120;
+let score = 0;
+let timerInterval;
+
+
+let audioCorrect = new Audio("assets/sfx/correct.wav");
+let audioIncorrect = new Audio("assets/sfx/incorrect.wav");
+
+//Starts the quiz and displays the first question 
+function startQuiz() {
+    startScreen.classList.add('hide');
+    quizQuestions.classList.remove('hide');
+    timerInterval = setInterval(function () {
+        timeLeft--;
+        timerDisplay.textContent = timeLeft;
+        if (timeLeft <= 0) {
+            timerDisplay.textContent = 0;
+            endQuiz();
+        }
+    }, 1000);
+    displayQuestion(questions[currentQuizQuestion]);
+}
+
+//Displays the current question and choices 
+function displayQuestion(question) {
+    questionTitle.textContent = question.question;
+    answerChoices.innerHTML = '';
+
+    for (let i = 0; i < question.choices.length; i++) {
+        let choice = question.choices[i];
+        let button = document.createElement('button');
+        button.textContent = choice;
+        button.setAttribute('data-index', i);
+        button.onclick = checkAnswer;
+        answerChoices.appendChild(button);
+    }
+}
+
+//Displays feedback message for answers depnding on if they were correct or not
+function showFeedback(message) {
+    feedbackMessage.textContent = message;
+    feedbackMessage.setAttribute('class', `feedback`);
+    setTimeout(function () {
+        feedbackMessage.setAttribute('class', 'feedback hide');
+    }, 1000);
+}
+
+//Checks if the answer is correct or not
+//If the answer is correct, add 10 points to the score, if not minus 10 secs from the timer
+function checkAnswer(e) {
+    let i = e.target.getAttribute('data-index');
+    let question = questions[currentQuizQuestion];    
+    if (question.choices[i] === question.choices[question.answer]) {
+        audioCorrect.play();
+        showFeedback('Correct!');       
+        score += 10;        
+    } else {
+        audioIncorrect.play();
+        showFeedback('Incorrect!');        
+        timeLeft -= 10;     
+    }
+
+    //Go to the next question, ff there are more questions, show the next one if not end the quiz
+    currentQuizQuestion++;
+    if (currentQuizQuestion < questions.length) {
+        displayQuestion(questions[currentQuizQuestion]);
+    } else {
+        endQuiz();
+    }
+}
+
